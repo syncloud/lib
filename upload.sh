587285fa4e03a33f3079617313f17c53ce279be7
Boris Rybalkin <ribalkin@gmail.com>
2017-12-01 17:18:27 +0000

    fix login var

--- upload.sh
+++ upload.sh
@@ -4,13 +4,17 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 export HOME=$DIR
 set +x
+if [ -z "$PYPI_LOGIN" ]; then
+  echo "PYPI_LOGIN must be set"
+  exit 1
+fi
 cat >$HOME/.pypirc <<EOF
 [distutils]
 index-servers = pypi
 
 [pypi]
-username: $PYPI_LOGIN
-password: $PYPI_PASSWORD
+username: ${PYPI_LOGIN}
+password: ${PYPI_PASSWORD}
 EOF
 set -x
 
