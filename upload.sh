958501b6d9bd8517cdc681a2b86bafb460a45035
Boris Rybalkin <ribalkin@gmail.com>
2017-12-01 17:04:46 +0000

    fix upload

--- upload.sh
+++ upload.sh
@@ -1,32 +1,17 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-app=platform
-branch=$1
-build_number=$2
-installer=$3
-bucket=apps.syncloud.org
-ARCH=$(uname -m)
+set +x
+cat >$HOME/.pypirc <<EOF
+[distutils]
+index-servers = pypi
 
-mkdir -p /opt/app
-SAMCMD=/opt/app/sam/bin/sam
+[pypi]
+username: $PYPI_LOGIN
+password: $PYPI_PASSWORD
+EOF
+set -x
 
-FILE_NAME=${app}-${build_number}-${ARCH}.tar.gz
-if [ $installer == "snapd" ]; then
-  ARCH=$(dpkg-architecture -q DEB_HOST_ARCH)
-  FILE_NAME=${app}_${build_number}_${ARCH}.snap
-fi
-
-if [ "${branch}" == "master" ] || [ "${branch}" == "stable" ] ; then
-
-  s3cmd put $FILE_NAME s3://${bucket}/apps/$FILE_NAME
-  
-  if [ "${branch}" == "stable" ]; then
-    branch=rc
-  fi
-
-  ${SAMCMD} release $branch $branch --override ${app}=${build_number}
-
-fi
-
+python setup.py sdist upload
+    
