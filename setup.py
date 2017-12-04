a51f626925155e90a3cabdbcd736a024684191f8
Boris Rybalkin <ribalkin@gmail.com>
2017-12-04 16:12:31 +0000

    jinja

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from os.path import join, dirname
 
 requirements = [
     'jsonpickle==0.7.1',
-    'requests==2.11.1'
+    'requests==2.11.1',
+    'Jinja2==2.9.6'
 ]
 
 version = open(join(dirname(__file__), 'version')).read().strip()
