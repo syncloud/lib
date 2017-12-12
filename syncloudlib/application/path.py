f6e9b2c06536075c0fda02673b5590e13ef48c96
Boris Rybalkin <ribalkin@gmail.com>
2017-12-12 10:15:10 +0000

    simplify path detection

--- syncloudlib/application/path.py
+++ syncloudlib/application/path.py
@@ -1,10 +1,26 @@
 import requests_unixsocket
 import json
+import os
 
 socket_file = '/opt/data/platform/api.socket'.replace('/', '%2F')
 socket = 'http+unix://{0}'.format(socket_file)
 
+
 def get_install_path(app):
+    if 'SNAP' in os.environ:
+        return '/snap/{0}/current'.format(app)
+    else:
+        return '/opt/app/{0}'.format(app)
+
+
+def get_data_dir(app):
+    if 'SNAP' in os.environ:
+        return '/var/snap/{0}/common'.format(app)
+    else:
+        return '/opt/data/{0}'.format(app)
+
+
+def _temp():
     session = requests_unixsocket.Session()
     try:
         response = session.get('{0}/app/install_path?name={1}'.format(socket, app))
