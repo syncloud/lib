ea355712f4b5f0b017d960a9721fe64a4c9e999e
Boris Rybalkin <ribalkin@gmail.com>
2018-01-24 16:30:15 +0000

    fix import

--- test/application/test_connection.py
+++ test/application/test_connection.py
@@ -1,4 +1,4 @@
-from syncloudlib.application.connection import request
+from syncloudlib.application.connection import api_get, api_post
 
 
 def test_import():
---
+++ test/application/test_storage.py
@@ -0,0 +1,5 @@
+from syncloudlib.application.storage import init_storage
+
+
+def test_import():
+    assert True
