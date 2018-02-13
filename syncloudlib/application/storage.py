8edd27273fd78c1a5a6dc146f470c333d350bf2c
Boris Rybalkin <ribalkin@gmail.com>
2018-02-13 18:58:22 +0000

    storage dir

--- syncloudlib/application/storage.py
+++ syncloudlib/application/storage.py
@@ -3,3 +3,6 @@ from connection import api_post
 
 def init_storage(app, user):
     return api_post('/app/init_storage', data={"app_name": app, "user_name": user})
+
+def get_storage_dir(app):
+    return api_get('/app/storage_dir', data={"app_name": app})
