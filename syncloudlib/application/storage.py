821180eb61bfc901fad6612a9558a20cc3ee90d8
Boris Rybalkin <ribalkin@gmail.com>
2018-02-13 21:40:43 +0000

    fix storage dir

--- syncloudlib/application/storage.py
+++ syncloudlib/application/storage.py
@@ -1,8 +1,9 @@
-from connection import api_post
+from connection import api_post, api_get
 
 
 def init_storage(app, user):
     return api_post('/app/init_storage', data={"app_name": app, "user_name": user})
 
+
 def get_storage_dir(app):
-    return api_get('/app/storage_dir', data={"app_name": app})
+    return api_get('/app/storage_dir?name={0}'.format(app))
