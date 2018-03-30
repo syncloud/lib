94296aec2a05311fe211a9c59ce82ef354cd3bae
Boris Rybalkin <ribalkin@gmail.com>
2018-03-30 16:11:53 +0100

    device domain name

--- syncloudlib/application/urls.py
+++ syncloudlib/application/urls.py
@@ -4,6 +4,8 @@ from connection import api_get
 def get_app_url(app):
     return api_get('/app/url?name={0}'.format(app))
 
+def get_device_domain_name():
+    return api_get('/app/device_domain_name')
 
 def get_app_domain_name(app):
     return api_get('/app/domain_name?name={0}'.format(app))
