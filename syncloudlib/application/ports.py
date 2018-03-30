d585e61caa4b7bb583112b827b94aad6656a695c
Boris Rybalkin <ribalkin@gmail.com>
2018-03-30 23:38:03 +0100

    ports

---
+++ syncloudlib/application/ports.py
@@ -0,0 +1,9 @@
+from connection import api_post
+
+
+def add_port(port, protocol):
+    return api_post('/port/add', data={"port": port, "protocol": protocol})
+
+
+def remove_port(port, protocol):
+    return api_post('/port/remove', data={"port": port, "protocol": protocol})
