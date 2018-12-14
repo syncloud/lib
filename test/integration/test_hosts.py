21040ec9bbed89902817cb0326ddd8cbfd4ccb44
Boris Rybalkin <ribalkin@gmail.com>
2018-12-14 09:13:39 +0000

    fix test

--- test/integration/test_hosts.py
+++ test/integration/test_hosts.py
@@ -1,8 +1,8 @@
 import os
-from syncloudlib.integration.hosts import add_hosts_alias
+from syncloudlib.integration.hosts import add_host_alias
 
 
-def test_add_hosts_alias():
+def test_add_host_alias():
 
     fd, path = tempfile.mkstemp()
     try:
