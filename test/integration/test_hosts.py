45344b38a287faf7f69b70a43c32be433397ae94
Boris Rybalkin <ribalkin@gmail.com>
2018-12-14 09:25:35 +0000

    fix line

--- test/integration/test_hosts.py
+++ test/integration/test_hosts.py
@@ -11,7 +11,7 @@ def test_add_host_alias():
         with os.fdopen(fd, 'r') as tmp:
             content = tmp.readlines()
             assert 'localhost.tld' in content[0]
-            assert 'app.localhost.tld' in content[0]
+            assert 'app.localhost.tld' in content[1]
     finally:
         os.remove(path)
     
