c6399d46fae52162080734bb0b32203a50eab016
Boris Rybalkin <ribalkin@gmail.com>
2018-12-14 09:11:33 +0000

    host alias

---
+++ test/integration/test_hosts.py
@@ -0,0 +1,16 @@
+import os
+from syncloudlib.integration.hosts import add_hosts_alias
+
+
+def test_add_hosts_alias():
+
+    fd, path = tempfile.mkstemp()
+    try:
+        add_hosts_alias('localhost', 'test', 'tld', path)
+        with os.fdopen(fd, 'r') as tmp:
+            content = tmp.readlines()
+            assert 'localhost.tld' in content[0]
+            assert 'test.localhost.tld' in content[0]
+    finally:
+        os.remove(path)
+    
