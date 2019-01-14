c99dd60e1eb0983f63482f118306468e24206d1f
Boris Rybalkin <ribalkin@gmail.com>
2019-01-14 20:45:58 +0000

    retrying login

--- syncloudlib/integration/device.py
+++ syncloudlib/integration/device.py
@@ -22,7 +22,21 @@ class Device():
                                        'device_password': self.device_password})
         return response
 
-    def login(self):
-        session = requests.session()
-        session.post('https://{0}/rest/login'.format(self.device_host), data={'name': self.device_user, 'password': self.device_password}, verify=False)
-        return session
+    def login(self, retries=5):
+    
+    
+        retry = 0
+        while True:
+            try:
+                session = requests.session()
+                session.post('https://{0}/rest/login'.format(device_host), verify=False, data={'name': self.device_user, 'password': self.device_password})
+                assert session.get('https://{0}/rest/user'.format(device_host), verify=False, allow_redirects=False).status_code == 200
+                return session
+            except Exception, e:
+                retry += 1
+                if retry > retries:
+                    raise e
+                print(e.message)
+                print('retry {0} of {1}'.format(retry, retries))
+
+ 
