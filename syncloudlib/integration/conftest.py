4fb7c86ec2447255f257548c70ecfacb28b19063
Boris Rybalkin <ribalkin@gmail.com>
2019-05-21 18:59:47 +0100

    ui test mode switch

--- syncloudlib/integration/conftest.py
+++ syncloudlib/integration/conftest.py
@@ -14,6 +14,7 @@ def pytest_addoption(parser):
     parser.addoption("--device-host", action="store")
     parser.addoption("--app-archive-path", action="store")
     parser.addoption("--app", action="store")
+    parser.addoption("--ui-mode", action="store", default="desktop")
 
 
 @pytest.fixture(scope='session')
@@ -39,6 +40,11 @@ def redirect_password():
 @pytest.fixture(scope='session')
 def app(request):
     return request.config.getoption("--app")
+
+
+@pytest.fixture(scope='session')
+def ui_mode(request):
+    return request.config.getoption("--ui-mode")
 
 
 @pytest.fixture(scope='session')
@@ -101,7 +107,7 @@ def new_profile(user_agent):
     return profile
 
 
-def new_driver(profile, log_dir):
+def new_driver(profile, log_dir, ui_mode):
 
     firefox_path = '/tools/firefox/firefox'
     caps = DesiredCapabilities.FIREFOX
@@ -110,26 +116,36 @@ def new_driver(profile, log_dir):
 
     binary = FirefoxBinary(firefox_path)
 
-    return webdriver.Firefox(profile, capabilities=caps, log_path="{0}/firefox.log".format(log_dir),
+    return webdriver.Firefox(profile, capabilities=caps, log_path="{0}/firefox.{1}.log".format(log_dir, ui_mode),
                              firefox_binary=binary, executable_path='/tools/geckodriver/geckodriver')
 
 
+
 @pytest.fixture(scope="module")
-def driver(log_dir):
+def desktop_driver(log_dir, ui_mode):
     profile = new_profile("Mozilla/5.0 (X11; Linux x86_64; rv:10.0) Gecko/20100101 Firefox/10.0")
-    driver = new_driver(profile, log_dir)
+    driver = new_driver(profile, log_dir, ui_mode)
     driver.set_window_position(0, 0)
     driver.set_window_size(1024, 2000)
     return driver
     
     
 @pytest.fixture(scope="module")
-def mobile_driver(log_dir):    
+def mobile_driver(log_dir, ui_mode):    
     profile = new_profile("Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_0 like Mac OS X; en-us) AppleWebKit/528.18 (KHTML, like Gecko) Version/4.0 Mobile/7A341 Safari/528.16")
-    driver = new_driver(profile, log_dir)
+    driver = new_driver(profile, log_dir, ui_mode)
     driver.set_window_position(0, 0)
     driver.set_window_size(400, 2000)
     return driver
+
+
+@pytest.fixture(scope="module")
+def driver(mobile_driver, desktop_driver, ui_mode):    
+    if ui_mode == "desktop":
+        return desktop_driver
+    else:
+        return mobile_driver
+    
 
 
 @pytest.fixture(scope="session")
