75c2423dd503767289ceff553997b8e8a7fea6dc
Boris Rybalkin <ribalkin@gmail.com>
2019-08-01 19:34:17 +0100

    more hosts functions

--- syncloudlib/integration/hosts.py
+++ syncloudlib/integration/hosts.py
@@ -1,7 +1,10 @@
 import socket
 
 def add_host_alias(app, host, domain='syncloud.info', hosts_file='/etc/hosts'):
-    ip = socket.gethostbyname(host)
+    add_host_alias_by_ip(app, host, host, domain, hosts_file))
+
+def add_host_alias_by_ip(app, host, host_for_ip, domain='syncloud.info', hosts_file='/etc/hosts'):
+    ip = socket.gethostbyname(host_for_ip)
     with open(hosts_file, "a") as hosts:
         hosts.write("{0} {1}.{2}\n".format(ip, host, domain))
         hosts.write("{0} {1}.{2}.{3}\n".format(ip, app, host, domain))
