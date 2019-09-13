5458a981c2b75b281f740f00d09eb26a6b5c8ef0
Boris Rybalkin <ribalkin@gmail.com>
2019-09-13 18:39:51 +0100

    http waiter simple url

--- syncloudlib/http.py
+++ syncloudlib/http.py
@@ -1,22 +1,22 @@
 import time
 
 
-def wait_for_rest(web_session, host, url, code, attempts=10):
+def wait_for_rest(web_session, url, code, attempts=10):
     def pred(resp):
         return resp.status_code == code
 
-    wait_for_response(web_session, host, url, pred, attempts)
+    wait_for_response(web_session, url, pred, attempts)
 
 
 
-def wait_for_response(web_session, host, url, resp_predicate, attempts=10):
+def wait_for_response(web_session, url, resp_predicate, attempts=10):
     
     attempt=0
     attempt_limit=attempts
     response = None
     while attempt < attempt_limit:
         try:
-            response = web_session.get('https://{0}{1}'.format(host, url), verify=False)
+            response = web_session.get(url, verify=False)
             print('code: {0}'.format(response.status_code))
             if resp_predicate(response):
                 return
