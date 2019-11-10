bbd10a21eb96e7e3fa182324cffdc862979dfa40
Boris Rybalkin <ribalkin@gmail.com>
2019-11-10 19:52:07 +0000

    fix imports

--- syncloudlib/integration/conftest.py
+++ syncloudlib/integration/conftest.py
@@ -1,6 +1,6 @@
 import pytest
 import os
-from os.path import join, exist
+from os.path import join, exists
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
