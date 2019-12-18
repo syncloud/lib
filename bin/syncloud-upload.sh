c3a203cfa27fda8be8fd9d86595d660389a93365
Boris Rybalkin <ribalkin@gmail.com>
2019-12-18 07:52:03 +0000

    check env variables

--- bin/syncloud-upload.sh
+++ bin/syncloud-upload.sh
@@ -1,5 +1,13 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 
+if [ -z "$AWS_ACCESS_KEY_ID" ]; then
+  echo "AWS_ACCESS_KEY_ID must be set"
+  exit 1
+fi
+if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
+  echo "AWS_SECRET_ACCESS_KEY must be set"
+  exit 1
+fi
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 app=$1
