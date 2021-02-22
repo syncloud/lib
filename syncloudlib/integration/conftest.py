3b99a7151f242ef570c76f52ddfea8be117caa46
Boris Rybalkin <ribalkin@gmail.com>
2021-02-22 22:42:52 +0000

    remote driver insecure cert

--- syncloudlib/integration/conftest.py
+++ syncloudlib/integration/conftest.py
@@ -143,11 +143,22 @@ def desktop_driver(log_dir, ui_mode):
     
 @pytest.fixture(scope="module")
 def remote_desktop_driver():
-    return webdriver.Remote(
-        command_executor='http://selenium:4444/wd/hub',
-        desired_capabilities={'browserName': 'firefox', 'javascriptEnabled': True}
-    )
+    profile = new_profile("Mozilla/5.0 (X11; Linux x86_64; rv:10.0) Gecko/20100101 Firefox/10.0")
 
+    caps = DesiredCapabilities.FIREFOX
+    #caps["marionette"] = True
+    caps['acceptSslCerts'] = True
+    caps['javascriptEnabled'] =True
+    caps['acceptInsecureCerts'] = True
+
+    driver = ebdriver.Remote(
+        command_executor='http://selenium:4444/wd/hub',
+        desired_capabilities=caps,
+        browser_profile=profile
+    )
+    driver.set_window_position(0, 0)
+    driver.set_window_size(1024, 2000)
+    return driver 
 
 @pytest.fixture(scope="module")
 def mobile_driver(log_dir, ui_mode):    
