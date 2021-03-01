d0b7abff3666ec63b29bf2cc88135d45025aa356
Boris Rybalkin <ribalkin@gmail.com>
2021-03-01 09:02:18 +0000

    selenium wrapper

--- syncloudlib/integration/conftest.py
+++ syncloudlib/integration/conftest.py
@@ -3,10 +3,10 @@ import os
 from os.path import join, exists
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
 from syncloudlib.integration.installer import get_data_dir, get_app_dir, get_service_prefix, get_ssh_env_vars, get_snap_data_dir
 from syncloudlib.integration.device import Device
+from syncloudlib.integration.selenium import Selenium
 
 SYNCLOUD_INFO = 'syncloud.info'
 
@@ -206,3 +206,6 @@ def screenshot_dir(artifact_dir):
     return dir
 
 
+@pytest.fixture(scope="module")
+def selenium(driver, ui_mode, screenshot_dir, app_domain):
+    return Selenium(driver, ui_mode, screenshot_dir, app_domain)
---
+++ syncloudlib/integration/selenium.py
@@ -0,0 +1,42 @@
+from syncloudlib.integration.screenshots import screenshots
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+
+
+class Selenium:
+    def __init__(self, driver, ui_mode, screenshot_dir, app_domain):
+        self.app_domain = app_domain
+        self.screenshot_dir = screenshot_dir
+        self.ui_mode = ui_mode
+        self.driver = driver
+
+    def find_by_xpath(self, xpath):
+        self.wait_or_screenshot(expected_conditions.presence_of_element_located((By.XPATH, xpath)))
+        return self.driver.find_element_by_xpath(xpath)
+
+    def find_by_name(self, name):
+        self.wait_or_screenshot(expected_conditions.presence_of_element_located((By.NAME, name)))
+        return self.driver.find_element_by_name(name)
+
+    def find_by_id(self, field_id):
+        self.wait_or_screenshot(expected_conditions.presence_of_element_located((By.ID, field_id)))
+        return self.driver.find_element_by_id(field_id)
+
+    def find_by_css(self, css):
+        self.wait_or_screenshot(expected_conditions.presence_of_element_located((By.CSS_SELECTOR, css)))
+        return self.driver.find_element_by_css(css)
+
+    def wait_or_screenshot(self, method):
+        wait_driver = WebDriverWait(self.driver, 30)
+        try:
+            wait_driver.until(method)
+        except Exception as e:
+            self.screenshot('exception')
+            raise e
+
+    def screenshot(self, name):
+        screenshots(self.driver, self.screenshot_dir, '{}-{}'.format(name, self.ui_mode))
+
+    def open_app(self):
+        self.driver.get("https://{0}".format(self.app_domain))
