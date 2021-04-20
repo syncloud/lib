26566af6ee4bf469b8bb9186123a30ad9d83efcb
Boris Rybalkin <ribalkin@gmail.com>
2021-04-20 22:32:03 +0100

    fix two parallel drivers

--- syncloudlib/integration/conftest.py
+++ syncloudlib/integration/conftest.py
@@ -136,30 +136,20 @@ def new_driver(profile):
 
 
 @pytest.fixture(scope="module")
-def desktop_driver(log_dir, ui_mode):
-    profile = new_profile("Mozilla/5.0 (X11; Linux x86_64; rv:10.0) Gecko/20100101 Firefox/10.0")
-    driver = new_driver(profile)
-    driver.set_window_rect(0, 0, 1024, 2000)
-    return driver
-    
-    
-@pytest.fixture(scope="module")
-def mobile_driver(log_dir, ui_mode):    
-    profile = new_profile(
-        "Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_0 like Mac OS X; en-us) "
-        "AppleWebKit/528.18 (KHTML, like Gecko) "
-        "Version/4.0 Mobile/7A341 Safari/528.16")
-    driver = new_driver(profile)
-    driver.set_window_rect(0, 0, 400, 2000)
-    return driver
-
-
-@pytest.fixture(scope="module")
-def driver(mobile_driver, desktop_driver, ui_mode):    
+def driver(ui_mode):    
     if ui_mode == "desktop":
-        return desktop_driver
+        profile = new_profile("Mozilla/5.0 (X11; Linux x86_64; rv:10.0) Gecko/20100101 Firefox/10.0")
+        driver = new_driver(profile)
+        driver.set_window_rect(0, 0, 1024, 2000)
+        return driver
     else:
-        return mobile_driver
+        profile = new_profile(
+            "Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_0 like Mac OS X; en-us) "
+            "AppleWebKit/528.18 (KHTML, like Gecko) "
+            "Version/4.0 Mobile/7A341 Safari/528.16")
+        driver = new_driver(profile)
+        driver.set_window_rect(0, 0, 400, 2000)
+        return driver
 
 
 @pytest.fixture(scope="session")
