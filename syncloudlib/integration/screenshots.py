386cba00b6499e5bab43ed3090a9f61305aa918a
Boris Rybalkin <ribalkin@gmail.com>
2021-04-21 06:55:32 +0100

    page source to str

--- syncloudlib/integration/screenshots.py
+++ syncloudlib/integration/screenshots.py
@@ -4,4 +4,4 @@ from os.path import join
 def screenshots(driver, screenshot_dir, name):
     driver.get_screenshot_as_file(join(screenshot_dir, '{0}.png'.format(name)))
     with open(join(screenshot_dir, '{0}.html.log'.format(name)), "w") as f:
-        f.write(driver.page_source.encode("utf-8"))
+        f.write(str(driver.page_source.encode("utf-8")))
