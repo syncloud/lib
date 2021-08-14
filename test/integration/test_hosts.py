45e4d9a9aff15cba31cb35b28d3de3bcaf92a4d3
Boris Rybalkin <ribalkin@gmail.com>
2021-08-14 10:06:09 +0300

    fix test

--- test/integration/test_hosts.py
+++ test/integration/test_hosts.py
@@ -10,8 +10,8 @@ def test_add_host_alias():
         add_host_alias('app', 'localhost', 'example.com', path)
         with os.fdopen(fd, 'r') as tmp:
             content = tmp.readlines()
-            assert content[0].endswith(' example.com')
-            assert content[1].endswith(' app.example.com')
+            assert content[0].endswith(' example.com\n')
+            assert content[1].endswith(' app.example.com\n')
     finally:
         os.remove(path)
     
