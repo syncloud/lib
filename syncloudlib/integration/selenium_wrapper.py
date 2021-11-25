aa7a0b36d8732968ea11c766ccadc908e4d7090d
Boris Rybalkin <ribalkin@gmail.com>
2021-11-25 20:42:47 +0000

    fix ui wait

--- syncloudlib/integration/selenium_wrapper.py
+++ syncloudlib/integration/selenium_wrapper.py
@@ -12,7 +12,7 @@ class SeleniumWrapper:
         self.screenshot_dir = screenshot_dir
         self.ui_mode = ui_mode
         self.driver = driver
-        self.wait_driver = WebDriverWait(self.driver, 30)
+        self.wait_driver = WebDriverWait(self.driver, 120)
 
     def find_by_xpath(self, xpath):
         self.wait_or_screenshot(expected_conditions.presence_of_element_located((By.XPATH, xpath)))
@@ -38,7 +38,7 @@ class SeleniumWrapper:
             raise e
 
     def screenshot(self, name):
-        retries = 120
+        retries = 5
         retry = 0
         while True:
             try:
