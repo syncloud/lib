12bedd1d0ea6e1b80415414bda2f5b89a3d0a2a5
Boris Rybalkin <ribalkin@gmail.com>
2024-04-21 16:22:00 +0100

    clickable fix

--- syncloudlib/integration/selenium_wrapper.py
+++ syncloudlib/integration/selenium_wrapper.py
@@ -41,11 +41,11 @@ class SeleniumWrapper:
 
     def clickable_by(self, by, value):
         self.wait_or_screenshot(expected_conditions.element_to_be_clickable((by, value)))
-        self.driver.find_element(by, value)
+        return self.driver.find_element(by, value)
 
     def present_by(self, by, value):
         self.wait_or_screenshot(expected_conditions.presence_of_element_located((by, value)))
-        self.driver.find_element(by, value)
+        return self.driver.find_element(by, value)
 
 
     def exists_by(self, by, value, timeout=10):
