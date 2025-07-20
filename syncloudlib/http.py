c8802cdbdf010836f0a035b616da6fb489093a17
Boris Rybalkin <ribalkin@gmail.com>
2025-07-20 17:07:30 +0100

    http logs

--- syncloudlib/http.py
+++ syncloudlib/http.py
@@ -1,4 +1,6 @@
 import time
+import logging
+log = logging.getLogger()
 
 
 def wait_for_rest(web_session, url, code, attempts=10):
@@ -16,13 +18,13 @@ def wait_for_response(web_session, url, resp_predicate, attempts=10):
     while attempt < attempt_limit:
         try:
             response = web_session.get(url, verify=False)
-            print('code: {0}'.format(response.status_code))
+            log.info('code: {0}'.format(response.status_code))
             if resp_predicate(response):
                 return
         except Exception as e:
-            print(str(e))
+            log.info(str(e))
         time.sleep(10)
         attempt = attempt + 1
     if response and response.text:
-        print(response.text)
+        log.info(response.text)
     raise Exception('exhausted')
