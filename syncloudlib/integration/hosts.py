616c3dc9472bb655b5c8111c86dfa424f339b433
Boris Rybalkin <ribalkin@gmail.com>
2025-07-20 12:41:13 +0100

    hosts log

--- syncloudlib/integration/hosts.py
+++ syncloudlib/integration/hosts.py
@@ -1,9 +1,12 @@
 import socket
+import logging
+log = logging.getLogger()
+log.setLevel(logging.DEBUG)
 
 def add_host_alias(app,  host, domain, hosts_file='/etc/hosts'):
     ip = socket.gethostbyname(host)
     with open(hosts_file, "a") as hosts:
-        print("adding hosts: {0} {1}".format(ip, domain))
+        log.info("adding hosts: {0} {1}".format(ip, domain))
         hosts.write("{0} {1}\n".format(ip, domain))
-        print("adding hosts: {0} {1}.{2}".format(ip, app, domain))
+        log.info("adding hosts: {0} {1}.{2}".format(ip, app, domain))
         hosts.write("{0} {1}.{2}\n".format(ip, app, domain))
